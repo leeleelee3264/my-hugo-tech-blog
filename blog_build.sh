1c9bb2ff580715b86200a57f28396156cdaf2b44
leeleelee3264 <absinthe4902@naver.com>
2022-10-03 13:22:44 +0900

    Modify blog bash shell script 2

--- blog_build.sh
+++ blog_build.sh
@@ -18,14 +18,14 @@ hugo -t fuji
 
 # commit and push build result to github.io
 cd public || exit
+# shellcheck disable=SC2094
+touch date.txt | date >> date.txt
 git add .
 git commit -m "Publish blog to github.io Date: $TODAY"
 git push origin master
 
 # commit and push posting resource
 cd ..
-# shellcheck disable=SC2094
-touch date.txt | date >> date.txt
 git add .
 git commit -m "Add posting resource after publishing Date: $TODAY"
 git push
